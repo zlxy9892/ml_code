9066b5f0470f6d213730759cc931a161e862b6d7
Neutron <Neutron>
2017-09-14 08:50:26 +0800

    0.1.0

--- tensorflow/t10.py
+++ tensorflow/t10.py
@@ -70,26 +70,10 @@ train_step = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(cross_entropy)
 # start session
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    for i in range(2000):
+    for i in range(2001):
         batch = mnist.train.next_batch(50)
         if i % 100 == 0:
-            train_accuracy = accuracy.eval(feed_dict={xs: batch[0], ys: batch[1], keep_prob: 1.0})
+            train_accuracy = accuracy.eval(feed_dict={xs:batch[0], ys:batch[1], keep_prob:1.0})
             print('step %d, training accuracy %g' % (i, train_accuracy))
         train_step.run(feed_dict={xs:batch[0], ys:batch[1], keep_prob: 0.5})
-
-    print('test accuracy %g' % accuracy.eval(feed_dict={xs:mnist.test.images, ys:mnist.test.labels, keep_prob:1.0}))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
