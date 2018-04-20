1b6d106ec51939b7306a8928f114a90d29d8dacc
zlxy9892 <zlxy9892@live.com>
2018-04-20 15:14:20 +0800

    update knn

--- basic_algorithm/knn/kNN.py
+++ basic_algorithm/knn/kNN.py
@@ -2,104 +2,46 @@
 
 import numpy as np
 import operator
-import matplotlib
-import matplotlib.pyplot as plt
 
+class knn_clf(object):
 
-# 产生数据集
-def createDataSet():
-    group = np.array([ [1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1] ])
-    labels = ['A', 'A', 'B', 'B']
-    return group, labels
+    def __init__(self, k=3):
+        self.k = k
 
-# k邻近算法主函数（knn）
-def classify0(inX, dataSet, labels, k):
-    # 求inX数据与所有训练数据集的距离
-    dataSetSize = dataSet.shape[0]      # 训练数据集的行数（训练样本数量)
-    diffMat = np.tile(inX, (dataSetSize, 1)) - dataSet      # tile 函数：复制（m行，n列）的inX
-    sqDiffMat = diffMat**2
-    sqDistances = sqDiffMat.sum(axis=1)
-    distances = sqDistances**0.5
+    def fit(self, x, y):
+        self.x_train = x
+        self.y_train = y
 
-    # 距离排序
-    sortedDistIndicies = distances.argsort()
+    def square_distance(self, v1, v2):
+        return np.sum(np.square(v1-v2))
 
-    # 选择出距离最小的k个点
-    classCount = {}     # 记录在距离最近的k个点钟，每个label对应的数量
-    for i in range(k):
-        voteIlabel = labels[sortedDistIndicies[i]]
-        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1      # Python中字典(Dictionary)中的get() 函数：返回指定键的值，如果值不在字典中返回默认值。
+    def vote(self, ys):
+        ys_unique = np.unique(ys)
+        vote_dict = {}
+        for y in ys:
+            if y not in vote_dict.keys():
+                vote_dict[y] = 1
+            else:
+                vote_dict[y] += 1
+        sorted_vote_dict = sorted(vote_dict.items(), key=operator.itemgetter(1), reverse=True)
+        return sorted_vote_dict[0][0]
 
-    # 给字典排序
-    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)     # 根据字典中的第二列的值，即values进行由大到小排序
+    def predict(self, x):
+        y_pred = []
+        for i in range(len(x)):
+            dist_arr = [self.square_distance(x[i], self.x_train[j]) for j in range(len(self.x_train))]
+            sorted_index = np.argsort(dist_arr)
+            top_k_index = sorted_index[:self.k]
+            y_pred.append(self.vote(ys=self.y_train[top_k_index]))
+        return np.array(y_pred)
 
-    return sortedClassCount[0][0]
-
-# 读取约会数据
-def file2matrix(filename):
-    fr = open(filename)
-    arrayOfLines = fr.readlines()
-    numberOfLines = len(arrayOfLines)
-    resMat = np.zeros((numberOfLines, 3))
-    classLabelVector = []
-    index = 0
-    for line in arrayOfLines:
-        line = line.strip()
-        listFromLine = line.split('\t')
-        resMat[index, :] = listFromLine[0:3]
-        classLabelVector.append(int(listFromLine[-1]))
-        index += 1
-    return resMat, classLabelVector
-
-# 数据可视化
-def visualizeData(dataMat, labels):
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.scatter(dataMat[:, 0], dataMat[:, 1],
-               c=15.0 * np.array(labels), s=15.0 * np.array(labels))
-    plt.show()
-
-# 数据归一化
-def autoNorm(dataSet):
-    minVals = dataSet.min(0)
-    maxVals = dataSet.max(0)
-    ranges = maxVals - minVals
-    normDataSet = np.zeros(np.shape(dataSet))
-    m = dataSet.shape[0]
-    normDataSet = dataSet - np.tile(minVals, (m, 1))
-    normDataSet = normDataSet / np.tile(ranges, (m, 1))
-    return normDataSet, ranges, minVals
-
-    print minVals
-
-# 针对约会数据的分类器，得到推测的误差，其中前10%作为测试集，后90%作为训练集
-def datingClassTest():
-    hoRatio = 0.1
-    datingDataMat, datingLabels = file2matrix('./data/datingTestSet2.txt')
-    normMat, ranges, minVals = autoNorm(datingDataMat)
-    m = normMat.shape[0]
-    numTestVecs = int(m * hoRatio)
-    errorCount = 0.0
-    for i in range(numTestVecs):
-        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
-        print 'the classifer came back with: %d, the real answer is: %d' %(classifierResult, datingLabels[i])
-        if classifierResult != datingLabels[i]:
-            errorCount += 1.0
-    print "the total error rate is: %f" %(errorCount / float(numTestVecs))
-
-
-
-# ---------------------------- main ---------------------------- #
-
-#group, labels = createDataSet()
-print '------------------ result ------------------'
-#res = classify0([0, 0], group, labels, 3)
-datingDataMat, datingLabels = file2matrix('./data/datingTestSet2.txt')
-print datingDataMat
-print datingLabels
-visualizeData(datingDataMat, datingLabels)
-normMat, ranges, minVals = autoNorm(datingDataMat)
-print normMat
-datingClassTest()
-
-
+    def score(self, y_true=None, y_pred=None):
+        if y_true is None and y_pred is None:
+            y_pred = self.predict(self.x_train)
+            y_true = self.y_train
+        score = 0.0
+        for i in range(len(y_true)):
+            if y_true[i] == y_pred[i]:
+                score += 1
+        score /= len(y_true)
+        return score
---
+++ basic_algorithm/knn/train.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# data preparation
+np.random.seed(314)
+data_size_1 = 300
+x1_1 = np.random.normal(loc=5.0, scale=1.0, size=data_size_1)
+x2_1 = np.random.normal(loc=4.0, scale=1.0, size=data_size_1)
+y_1 = [0 for _ in range(data_size_1)]
+
+data_size_2 = 400
+x1_2 = np.random.normal(loc=10.0, scale=2.0, size=data_size_2)
+x2_2 = np.random.normal(loc=8.0, scale=2.0, size=data_size_2)
+y_2 = [1 for _ in range(data_size_2)]
+
+x1 = np.concatenate((x1_1, x1_2), axis=0)
+x2 = np.concatenate((x2_1, x2_2), axis=0)
+x = np.hstack((x1.reshape(-1,1), x2.reshape(-1,1)))
+y = np.concatenate((y_1, y_2), axis=0)
+
+data_size_all = data_size_1+data_size_2
+shuffled_index = np.random.permutation(data_size_all)
+x = x[shuffled_index]
+y = y[shuffled_index]
+
+split_index = int(data_size_all*0.7)
+x_train = x[:split_index]
+y_train = y[:split_index]
+x_test = x[split_index:]
+y_test = y[split_index:]
+
+# visualize data
+plt.scatter(x_train[:,0], x_train[:,1], c=y_train, marker='.')
+plt.show()
+plt.scatter(x_test[:,0], x_test[:,1], c=y_test, marker='.')
+plt.show()
+
+# data normalization
+x_train = (x_train - np.min(x_train, axis=0)) / (np.max(x_train, axis=0) - np.min(x_train, axis=0))
+x_test = (x_test - np.min(x_test, axis=0)) / (np.max(x_test, axis=0) - np.min(x_test, axis=0))
+
+# knn model
+import knn
+clf = knn.knn_clf(k=3)
+clf.fit(x_train, y_train)
+
+print('train accuracy: {:.3}'.format(clf.score()))
+
+y_test_pred = clf.predict(x_test)
+print('test accuracy: {:.3}'.format(clf.score(y_test, y_test_pred)))
